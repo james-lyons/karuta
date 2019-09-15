ba8a8b47106f61713d776896a5e9879a5d28ed80
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2019-09-15 10:05:01 -0700

    Updates

---
+++ precache-manifest.d97f4b0a59782264cbde8cfd8b768cae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c14c49d6fba73dbaf84c6af34e44e09",
+    "url": "/karuta/index.html"
+  },
+  {
+    "revision": "689b8070773713ad2dba",
+    "url": "/karuta/static/css/main.d37f57c5.chunk.css"
+  },
+  {
+    "revision": "8e3cf06eef530a0177d7",
+    "url": "/karuta/static/js/2.fb756864.chunk.js"
+  },
+  {
+    "revision": "689b8070773713ad2dba",
+    "url": "/karuta/static/js/main.8aa175d0.chunk.js"
+  },
+  {
+    "revision": "32dd518664c8ab635719",
+    "url": "/karuta/static/js/runtime~main.1db7b0fe.js"
+  },
+  {
+    "revision": "10142b2942b7c84f91de8a1043e6544d",
+    "url": "/karuta/static/media/karuta-2.10142b29.jpg"
+  },
+  {
+    "revision": "3f8141d12dc88fdc8b4c075b2f744046",
+    "url": "/karuta/static/media/karuta-3.3f8141d1.jpg"
+  },
+  {
+    "revision": "3f13e953719150d366f39e04534e78b3",
+    "url": "/karuta/static/media/karuta-5.3f13e953.jpg"
+  },
+  {
+    "revision": "e37752a049bfc0fea24376909ca97eca",
+    "url": "/karuta/static/media/karuta-background-1.e37752a0.jpg"
+  }
+]);
